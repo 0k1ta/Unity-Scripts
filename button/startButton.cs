20e227c2170f5b251dcceb1578c1e1f35b657848
okita <59220768+0k1ta@users.noreply.github.com>
2021-01-04 12:51:16 +0900

    Update startButton.cs

--- button/startButton.cs
+++ button/startButton.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class startButton : MonoBehaviour {
-
+  
   void Start () {
     // ボタンクリックでStartGame関数を実行
     gameObject.GetComponent<Button>().onClick.AddListener(StartGame);
